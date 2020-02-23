36287d017b5f8584e0416d9c76331377411c4a5d
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-23 14:29:46 -0800

    Core code

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrophy <dbrophy@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/22 17:16:16 by dbrophy           #+#    #+#             */
+/*   Updated: 2020/02/22 17:16:16 by dbrophy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1
+
+int				get_next_line(const int fd, char **line);
+
+#endif
